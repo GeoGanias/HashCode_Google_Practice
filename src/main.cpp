33d1617fd910d9bb1e2160770d5d8fb0d1166215
iliaspap123 <iliaspap97@gmail.com>
2020-02-10 18:16:51 +0200

    solution1

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <fstream>
+#include <cmath>
 
 using namespace std;
 
 int main(void) {
-  ifstream infile("./input/a_example.in");
+  ifstream infile("./input/e_also_big.in");
   int M,N;
   infile >> M >> N;
 
@@ -15,11 +16,38 @@ int main(void) {
   }
 
   bool *b1 = new bool[N];
-  int bestB = new bool[N];
-  int i = 0;
-  while( ) {
-    
+  int prevSum = 0;
+  int sum = 0;
+  bool *bestB = new bool[N];
+  i = 0;
+
+  for(i=0;i<pow(2.0,(double) N);i++) {
+    int j;
+    // for(j=0;j<N;j++) {
+    //   cout << b1[j];
+    // }
+    //cout << endl;
+    for(j=0;j<N;j++) {
+      if(b1[j] == false) {
+        sum += arr[j];
+        b1[j] = true;
+        break;
+      }
+      else {
+        sum -= arr[j];
+        b1[j] = false;
+      }
+    }
+    if(sum == M) {
+      prevSum = sum;
+      break;
+    }
+    else if(sum > M) {
+      break;
+    }
+    prevSum = sum;
   }
 
+  cout << "score " << prevSum <<endl;
   return 0;
 }
